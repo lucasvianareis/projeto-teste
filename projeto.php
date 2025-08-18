4edb934c99249ef0ad1e9c0483169d34952bc33e
Lucas Viana Reis <lucasvianareiss@gmail.com>
2025-08-17 21:15:11 -0300

    Ajustado pedido 1977

--- projeto.php
+++ projeto.php
@@ -4,5 +4,7 @@ $nomeCliente = "Lucas Viana Reis";
 $precoGasto = "R$100";
 
 
-echo "O nome do Cliente é: $nomeCliente e ele gastou exatamente: " . $precoGasto ;
+echo "O nome do Cliente é: $nomeCliente e ele gastou exatamente: " . $precoGasto . "\n";
+echo $nomeCliente . " Você deseja fazer algum pedido? \n ";
+echo"Se quiser fazer algum pedido digite 1";
 
