a3a8b1915369e71a344050bca1fc4438927c272f
Lucas Viana Reis <lucasvianareiss@gmail.com>
2025-08-17 21:03:25 -0300

    ajustado algumas informações

--- projeto.php
+++ projeto.php
@@ -1,7 +1,8 @@
 <?php 
 
 $nomeCliente = "Lucas Viana Reis";
+$precoGasto = "R$100";
 
 
+echo "O nome do Cliente é: $nomeCliente e ele gastou exatamente: " . $precoGasto ;
 
-echo "O nome do Cliente é: $nomeCliente ";
