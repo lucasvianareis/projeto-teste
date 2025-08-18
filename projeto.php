38d4e9945addbe854a27fdfcaee9436174a68adb
Lucas Viana Reis <lucasvianareiss@gmail.com>
2025-08-17 21:27:58 -0300

    Adicionado novos campos

--- projeto.php
+++ projeto.php
@@ -7,6 +7,6 @@ $precoGasto = "R$100";
 echo "O nome do Cliente é: $nomeCliente e ele gastou exatamente: " . $precoGasto . "\n";
 echo $nomeCliente . " Você deseja fazer algum pedido? \n ";
 echo"Se quiser fazer algum pedido digite 1 \n";
-echo "So mais uma coisa";
-
+echo "So mais uma coisa \n";
+echo"NOVO TESTE ACONTECENDO AQUI"
 
